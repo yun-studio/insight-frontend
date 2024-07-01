362e1e804b5e53694b7d958cf14c4aaf316909fb
barami62 <wodbs7758@naver.com>
2024-07-01 22:37:59 +0900

    Fix: 검색창 상태를 컴포넌트 내부로 숨기도록 메인 페이지 수정

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,30 +1,11 @@
 import Layout from "../component/layout/Layout.jsx";
-import {Box} from "@mui/material";
-import styled from "@emotion/styled";
-import {useState} from "react";
 import SearchBar from "../component/SearchBar.jsx";
-import QuestionList from "../component/QuestionList.jsx";
-
-const CustomBox = styled(Box)`
-`
 
 function MainPage() {
 
-    const [questions, setQuestions] = useState([])
-
     return (
         <Layout>
-            <CustomBox>
-                <Box component={"h1"}>Hello world</Box>
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}>
-                    <SearchBar setQuestions={setQuestions}/>
-                </Box>
-                <QuestionList questions={questions}/>
-            </CustomBox>
+            <SearchBar/>
         </Layout>
     );
 }
