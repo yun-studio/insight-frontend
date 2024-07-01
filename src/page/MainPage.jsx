ec0ee75ebfa5bd80f5c8d93428df20c0eb398cbd
barami62 <wodbs7758@naver.com>
2024-07-01 19:16:38 +0900

    Feat: 메인페이지에 검색 기능 추가

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,16 +1,29 @@
 import Layout from "../component/layout/Layout.jsx";
 import {Box} from "@mui/material";
 import styled from "@emotion/styled";
+import {useState} from "react";
+import SearchBar from "../component/SearchBar.jsx";
+import QuestionList from "../component/QuestionList.jsx";
 
-const CustomBox = styled('div')`
+const CustomBox = styled(Box)`
 `
 
 function MainPage() {
+
+    const [questions, setQuestions] = useState([])
 
     return (
         <Layout>
             <CustomBox>
                 <Box component={"h1"}>Hello world</Box>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <SearchBar setQuestions={setQuestions}/>
+                </Box>
+                <QuestionList questions={questions}/>
             </CustomBox>
         </Layout>
     );
