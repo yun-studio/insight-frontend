a131c07b2b87c54daf4d0588bab592724a8fc561
barami62 <wodbs7758@naver.com>
2024-07-01 22:38:59 +0900

    Feat: 질문 검색 결과 페이지 라우팅 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./page/MainPage.jsx";
+import QuestionPage from "./page/QuestionPage.jsx";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
+                <Route path="/questions" element={<QuestionPage/>}/>
             </Routes>
         </BrowserRouter>
     )
