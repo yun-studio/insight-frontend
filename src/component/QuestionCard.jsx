2b23f41f9d7af57d42a840877693765b80027942
barami62 <wodbs7758@naver.com>
2024-07-02 13:46:21 +0900

    Style: 질문 카드 스타일 보강

--- src/component/QuestionCard.jsx
+++ src/component/QuestionCard.jsx
@@ -3,7 +3,13 @@ import {Card, CardContent, Typography} from "@mui/material";
 function QuestionCard({data}) {
 
     return (
-        <Card>
+        <Card
+            sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                     {data.content}
